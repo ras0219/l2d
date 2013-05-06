a7ad5d6eedc138a5522d33ab947afb9a0843ba6e
phgdragon <peihongguo@gmail.com>
2013-05-05 23:20:57 -0500

    fixed the reference to the node being deleted

--- node.js
+++ node.js
@@ -251,7 +251,7 @@ function createNode(predefid, curdefid){
             // this needs to call a function in the layer, which will delete 
             // this node and remove it from all other node's lists
             disconnectNode(this.owner_node);
-            removeNode(node.id);
+            removeNode(this.owner_node.id);
     });
 
     this.del.on("mousedown", function(){    //all but output kind??
